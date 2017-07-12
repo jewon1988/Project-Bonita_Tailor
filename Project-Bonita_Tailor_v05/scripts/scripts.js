6db5f28f6cb2acd85ee4d5a9b2f01b41b32c6024
Jewon Moon <jewon1988@gamil.com>
2017-07-12 17:39:07 -0400

    Add Projects

---
+++ Project-Bonita_Tailor_v05/scripts/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+  $("#banner-slide").owlCarousel({
+ 
+      autoPlay: 6000, //Set AutoPlay to 3 seconds
+      singleItem:true
+      //navigation:true,
+      //navigationText:["", ""]
+  });
+ 
+});
